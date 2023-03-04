4f133731f558410a85d52e265320bdd82878eb32
Łukasz Żarski <lukasz.zarski@morizon-gratka.pl>
2023-03-04 20:24:52 +0100

    feat(field)

--- src/api/specificity/routes/specificity.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * specificity router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::specificity.specificity');
--- src/api/specificity/services/specificity.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * specificity service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::specificity.specificity');
