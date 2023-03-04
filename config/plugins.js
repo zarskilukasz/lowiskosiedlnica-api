d7562b1bf1389a3197b19c1361e27610f0b04a37
rchmielewski <robert.chmielewski94@gmail.com>
2023-03-04 01:06:20 +0100

    Add config/plugin.js

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'lukas.zarski@gmail.com',
+        defaultReplyTo: 'lukas.zarski@gmail.com',
+      },
+    },
+  },
+});
