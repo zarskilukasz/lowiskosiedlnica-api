9ed93300166a97c58141dcbdbeb9117fe16f7a46
Łukasz Żarski <lukasz.zarski@morizon-gratka.pl>
2023-03-06 11:56:05 +0100

    feat

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'http://ns3053871.ip-137-74-1.eu'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
