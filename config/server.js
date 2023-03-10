6662e2f5fc9ec3021e8ad8613b5b06f4735841be
Łukasz Żarski <lukasz.zarski@morizon-gratka.pl>
2023-03-10 16:29:49 +0100

    feat

--- config/server.js
+++ config/server.js
@@ -7,5 +7,5 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: env('URL','https://lowiskosiedlnica-api.gdzienaryby.com.pl'),
+  // url: env('URL','https://lowiskosiedlnica-api.gdzienaryby.com.pl'),
 });
